d513971ff84c1bd0ef5d1607507d83a450a9df29
ryan-txn <tanryan2002@gmail.com>
2024-05-29 17:25:01 +0800

    Sprite for Door to game

--- Dungeon proj/Assets/Scripts/SceneTransition.cs
+++ Dungeon proj/Assets/Scripts/SceneTransition.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class SceneTransition : MonoBehaviour
 {
     [SerializeField]
-    public string sceneToLoad;
+    public string _sceneToLoad;
+
+    private bool _doorAccessed;
 
     public void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "Player")
+        if (collider.gameObject.tag == "Player" /*&& _doorAccessed*/)
         {
-            SceneManager.LoadScene(sceneToLoad);
+            SceneManager.LoadScene(_sceneToLoad);
         }
+    }
+
+    private void OnInteract(InputValue inputValue)
+    {
+        _doorAccessed = inputValue.isPressed;
     }
 }
