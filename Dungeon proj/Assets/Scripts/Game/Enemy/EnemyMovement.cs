c060fb3428ced3ccb5779548afaf54ba7426ccb1
serweiq <serweiq@gmail.com>
2024-06-05 00:47:20 +0800

    Enemy idle animation when player leaves awareness zone
    - enemy enters idle animation
    - enemy turns the other way when moving again
    - gummybear and skelebread idle aniamtions added, graphics updated

--- Dungeon proj/Assets/Scripts/Game/Enemy/EnemyMovement.cs
+++ Dungeon proj/Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -7,16 +7,17 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField]
     private float _speed;
-
-    [SerializeField]
-    private float _screenBorder;
-
+    
     private Rigidbody2D _rigidbody;
     private PlayerAwarenessController _playerAwarenessController;
     private Vector2 _targetDirection;
     private float _changeDirectionCooldown; // time to change direction
     private float _wallDetectionCooldown; // time to change direction when colliding with wall
     private Camera _camera;
+    private bool _isIdle; // Flag to track idle state
+    private float _idleTimer; // Timer for idle duration
+
+    private Animator _animator;
 
     private void Awake()
     {
@@ -24,46 +25,71 @@ public class EnemyMovement : MonoBehaviour
         _playerAwarenessController = GetComponent<PlayerAwarenessController>();
         _targetDirection = transform.up; // initial target direction will be the way it's currently facing
         _camera = Camera.main;
+        _animator = GetComponentInChildren<Animator>();
     }
 
     private void FixedUpdate()
     {
-        UpdateTargetDirection();
-        SetVelocity();
-        UpdateSpriteDirection();
+        SetAnimation();
+        if (_isIdle)
+        {
+            _idleTimer -= Time.deltaTime;
+            if (_idleTimer <= 0)
+            {
+                _isIdle = false;
+                HandleRandomDirectionChange();
+            }
+        }
+        else
+        {
+            UpdateTargetDirection();
+            SetVelocity();
+            UpdateSpriteDirection();
+        }
     }
 
-    private void UpdateTargetDirection() 
+    private void SetAnimation()
     {
-        HandleRandomDirectionChange();
-        HandlePlayerTargeting();
+        _animator.SetBool("IsIdle", _isIdle);
+    }
+
+    private void UpdateTargetDirection()
+    {
+        if (_playerAwarenessController.AwareOfPlayer)
+        {
+            _targetDirection = _playerAwarenessController.DirectionToPlayer;
+            _isIdle = false; // Reset idle timer if player is detected
+        }
+        else if (!_isIdle) // Only check for random direction change if not already idling
+        {
+            HandleRandomDirectionChange();
+        }
     }
 
     private void HandleRandomDirectionChange()
     {
         _changeDirectionCooldown -= Time.deltaTime;
 
-        if (_changeDirectionCooldown <= 0)
-        {
-            float angleChange = Random.Range(-90f, 90f);
-            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
-            _targetDirection = rotation * _targetDirection;
+            if (_changeDirectionCooldown <= 0)
+            {
+                float angleChange = Random.Range(-90f, 90f);
+                Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
+                _targetDirection = rotation * _targetDirection;
 
-            _changeDirectionCooldown = Random.Range(1f, 5f);
-        }
-    }
-
-    private void HandlePlayerTargeting()
-    {
-        if (_playerAwarenessController.AwareOfPlayer)
-        {
-            _targetDirection = _playerAwarenessController.DirectionToPlayer;
-        }
+                _changeDirectionCooldown = Random.Range(1f, 5f);
+            }
     }
 
     private void SetVelocity() 
     {
-        _rigidbody.velocity = _targetDirection.normalized * _speed;
+        if (!_isIdle)
+        {
+            _rigidbody.velocity = _targetDirection.normalized * _speed;
+        }
+        else
+        {
+            _rigidbody.velocity = Vector2.zero; // Set velocity to zero while idling
+        }
     }
 
     private void UpdateSpriteDirection()
@@ -81,12 +107,19 @@ public class EnemyMovement : MonoBehaviour
     private void OnCollisionStay2D(Collision2D collision)
     {
         _wallDetectionCooldown -= Time.deltaTime;
-        if (collision.gameObject.GetComponent<TilemapCollider2D>() && _wallDetectionCooldown <= 0)
+       if (collision.gameObject.GetComponent<TilemapCollider2D>() && _wallDetectionCooldown <= 0)
         {
             Debug.Log("Enemy collided with Wall!");
-            _targetDirection = -_targetDirection;
+            _targetDirection = - _targetDirection;
 
             _wallDetectionCooldown = 1f;
         }
+    }
+
+    public void OnPlayerLeaveAwareness() // Call this function from PlayerAwarenessController when player leaves awareness zone
+    {
+        _isIdle = true;
+        _idleTimer = 2f; // Set idle timer for 2 seconds
+        _targetDirection = - _targetDirection;
     }
 }
--- Dungeon proj/Assets/Scripts/Game/Enemy/PlayerAwarenessController.cs
+++ Dungeon proj/Assets/Scripts/Game/Enemy/PlayerAwarenessController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerAwarenessController : MonoBehaviour
 {
@@ -14,11 +15,13 @@ public class PlayerAwarenessController : MonoBehaviour
     private Transform _playerTransform;
     private GameObject _playerObject;
     private bool hasLineOfSight;
+    private EnemyMovement _enemyMovement;
 
     private void Awake()
     {
         _playerTransform = FindObjectOfType<PlayerMovement>().transform;
         _playerObject = GameObject.FindGameObjectWithTag("Player");
+        _enemyMovement = GetComponent<EnemyMovement>();
     }
 
     // Update is called once per frame
@@ -26,18 +29,24 @@ public class PlayerAwarenessController : MonoBehaviour
     {
         HandleLineOfSight();
 
-        //vector of how far player is and in what direction
+        // Vector of how far player is and in what direction
         Vector2 enemyToPlayerVector = _playerTransform.position - transform.position;
-        //direction, normalized mmagnitude
+        // Direction, normalized magnitude
         DirectionToPlayer = enemyToPlayerVector.normalized;
 
         if ((enemyToPlayerVector.magnitude <= _playerAwarenessDistance) && hasLineOfSight)
         {
             Debug.DrawRay(transform.position, _playerObject.transform.position - transform.position, Color.green);
             AwareOfPlayer = true;
-        }   
-        else {
+        }
+        else
+        {
             Debug.DrawRay(transform.position, _playerObject.transform.position - transform.position, Color.red);
+            // Call the event when player leaves awareness
+            if (AwareOfPlayer)
+            {
+                _enemyMovement.OnPlayerLeaveAwareness();
+            }
             AwareOfPlayer = false;
         }
     }
@@ -45,7 +54,7 @@ public class PlayerAwarenessController : MonoBehaviour
     private void HandleLineOfSight()
     {
         RaycastHit2D ray = Physics2D.Raycast(transform.position, _playerObject.transform.position - transform.position);
-        
+
         if (ray.collider != null)
         {
             hasLineOfSight = ray.collider.CompareTag("Player");
