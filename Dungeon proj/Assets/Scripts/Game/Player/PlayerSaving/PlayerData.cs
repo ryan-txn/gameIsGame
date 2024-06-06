e5895e1fbb88205cd43a6ac0fca1a57863c4e74a
ryan-txn <tanryan2002@gmail.com>
2024-06-06 16:10:12 +0800

    save system part 1
    save and load scripts, not assigned to anything yet

---
+++ Dungeon proj/Assets/Scripts/Game/Player/PlayerSaving/PlayerData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public int coins;
+    public float health;
+
+    public PlayerData (CoinController playerCoins, HealthController playerHealth)
+    {
+        coins = playerCoins.coinAmt;
+        health = playerHealth.currentHealthNum;
+    }
+}
