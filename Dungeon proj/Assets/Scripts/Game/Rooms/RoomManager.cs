10bee21c5a6e6242ef0b82bbb1f0c7a4bd1ffbaf
ryan-txn <tanryan2002@gmail.com>
2024-06-25 21:12:53 +0800

    Fixed bug
    fixed issue that caused prefab to be edited. also made the 4 directions clearer in code.

--- Dungeon proj/Assets/Scripts/Game/Rooms/RoomManager.cs
+++ Dungeon proj/Assets/Scripts/Game/Rooms/RoomManager.cs
@@ -26,95 +26,132 @@ public class RoomManager : MonoBehaviour
         startRoomWalls.CompressBounds();
         int gridWidth = startRoomWalls.size.x;
         int gridHeight = startRoomWalls.size.y;
-        Vector3 spawnPoint = new Vector3(0, 0, 0);
-        int positionIndex = Random.Range(0, 4);
 
+        Vector3 spawnPoint = new Vector3(0, 0, 0);
+        string wallTilemapTag = "Wall";
+
+        int positionIndex = 3; //Random.Range(0, 4);
         var direction = Direction3D.GetRandomCardinalDirection(positionIndex);
 
-        string wallTilemapTag = "Wall";
-        Tilemap wallTilemap = FindTilemapWithTag(roomPrefabs[0], wallTilemapTag);
-        wallTilemap.CompressBounds();
-        int enmyGridWidth = wallTilemap.size.x;
-        int enmyGridHeight = wallTilemap.size.y;
-        Debug.Log(enmyGridHeight + ", " + enmyGridWidth);
-
-
-
-        if (positionIndex == 0 || positionIndex == 1)
+        if (positionIndex == 0) //ROOM ON TOP
         {
-            if (positionIndex == 0)
-            {
-                //delete walls
-                Vector3Int topGap1 = new (-1, (gridHeight / 2) - 1, 0);
-                Vector3Int topGap2 = new (0, (gridHeight / 2) - 1, 0);
-                startRoomWalls.SetTile(topGap1, null);
-                startRoomWalls.SetTile(topGap2, null);
+            spawnPoint += (direction * 13);
+            GameObject RoomInstance = Instantiate(roomPrefabs[0], spawnPoint, Quaternion.identity);
 
-                Vector3Int enmyBottomGap1 = new(-1, -(enmyGridHeight / 2), 0);
-                Vector3Int enmyBottomGap2 = new(0, -(enmyGridHeight / 2), 0);
-                Debug.Log(enmyBottomGap1 + ", " + enmyBottomGap2); // shld be -8
-                wallTilemap.CompressBounds();
-                wallTilemap.SetTile(enmyBottomGap1, null);
-                wallTilemap.SetTile(enmyBottomGap2, null);
-            } else {
-                //delete walls
-                Vector3Int bottomGap1 = new(-1, -(gridHeight / 2), 0);
-                Vector3Int bottomGap2 = new(0, -(gridHeight / 2), 0);
-                startRoomWalls.SetTile(bottomGap1, null);
-                startRoomWalls.SetTile(bottomGap2, null);
+            //delete room 0 walls
+            Vector3Int topGap1 = new(-1, (gridHeight / 2) - 1, 0);
+            Vector3Int topGap2 = new(0, (gridHeight / 2) - 1, 0);
+            startRoomWalls.SetTile(topGap1, null);
+            startRoomWalls.SetTile(topGap2, null);
 
-                Vector3Int enmyTopGap1 = new(-1, (enmyGridHeight / 2) - 1, 0);
-                Vector3Int enmyTopGap2 = new(0, (enmyGridHeight / 2) - 1, 0); 
-                Debug.Log(enmyTopGap1 + ", " + enmyTopGap2); // shld be 7
-                wallTilemap.CompressBounds();
-                wallTilemap.SetTile(enmyTopGap1, null);
-                wallTilemap.SetTile(enmyTopGap2, null);
-            }
+            //find room 1 wall tilemap
+            Tilemap wallTilemap = FindTilemapWithTag(RoomInstance, wallTilemapTag);
 
-            for (int i = 0; i < 13; i++)
-            {
-                spawnPoint += direction;
-            }
-            Instantiate(roomPrefabs[0], spawnPoint, Quaternion.identity);
+            //checking room 1 wall tilemap dimensions
+            wallTilemap.CompressBounds();
+            int enmyGridWidth = wallTilemap.size.x;
+            int enmyGridHeight = wallTilemap.size.y;
+            Debug.Log(enmyGridHeight + ", " + enmyGridWidth);
+
+            //checking room 1 wall tilemap tiles to be deleted
+            Vector3Int enmyBottomGap1 = new(-1, -(enmyGridHeight / 2), 0);
+            Vector3Int enmyBottomGap2 = new(0, -(enmyGridHeight / 2), 0);
+            Debug.Log(enmyBottomGap1 + ", " + enmyBottomGap2); // shld be -8
+
+            //delete appropriate room 1 walls
+            wallTilemap.CompressBounds();
+            wallTilemap.SetTile(enmyBottomGap1, null);
+            wallTilemap.SetTile(enmyBottomGap2, null);
         }
-        else
+        else if (positionIndex == 1)
+        { //ROOM ON BOTTOM
+            spawnPoint += (direction * 13);
+            GameObject RoomInstance = Instantiate(roomPrefabs[0], spawnPoint, Quaternion.identity);
+
+            //delete room 0 walls
+            Vector3Int bottomGap1 = new(-1, -(gridHeight / 2), 0);
+            Vector3Int bottomGap2 = new(0, -(gridHeight / 2), 0);
+            startRoomWalls.SetTile(bottomGap1, null);
+            startRoomWalls.SetTile(bottomGap2, null);
+
+            //find room 1 wall tilemap
+            Tilemap wallTilemap = FindTilemapWithTag(RoomInstance, wallTilemapTag);
+
+            //checking room 1 wall tilemap dimensions
+            wallTilemap.CompressBounds();
+            int enmyGridWidth = wallTilemap.size.x;
+            int enmyGridHeight = wallTilemap.size.y;
+            Debug.Log(enmyGridHeight + ", " + enmyGridWidth);
+
+            //checking room 1 wall tilemap tiles to be deleted
+            Vector3Int enmyTopGap1 = new(-1, (enmyGridHeight / 2) - 1, 0);
+            Vector3Int enmyTopGap2 = new(0, (enmyGridHeight / 2) - 1, 0);
+            Debug.Log(enmyTopGap1 + ", " + enmyTopGap2); // shld be 7
+
+            //delete appropriate room 1 walls
+            wallTilemap.CompressBounds();
+            wallTilemap.SetTile(enmyTopGap1, null);
+            wallTilemap.SetTile(enmyTopGap2, null);
+        }
+        else if (positionIndex == 2) //ROOM ON RIGHT
         {
-            if (positionIndex == 2)
-            {
-                //delete walls
-                Vector3Int rightGap1 = new Vector3Int((gridWidth / 2) - 1, 0, 0);
-                Vector3Int rightGap2 = new Vector3Int((gridWidth / 2) - 1, -1, 0);
-                startRoomWalls.SetTile(rightGap1, null);
-                startRoomWalls.SetTile(rightGap2, null);
+            spawnPoint += (direction * 16);
+            GameObject RoomInstance = Instantiate(roomPrefabs[0], spawnPoint, Quaternion.identity);
 
-                Vector3Int enmyRightGap1 = new Vector3Int(-(enmyGridWidth / 2), 0, 0);
-                Vector3Int enmyRightGap2 = new Vector3Int(-(enmyGridWidth / 2), -1, 0);
-                wallTilemap.CompressBounds();
-/*                Debug.Log(enmyRightGap1 + ", " + enmyRightGap2);
-*/                wallTilemap.SetTile(enmyRightGap1, null);
-                wallTilemap.SetTile(enmyRightGap2, null);
-            }
-            else
-            {
-                //delete walls
-                Vector3Int leftGap1 = new Vector3Int(-(gridWidth / 2), 0, 0);
-                Vector3Int leftGap2 = new Vector3Int(-(gridWidth / 2), -1, 0);
-                startRoomWalls.SetTile(leftGap1, null);
-                startRoomWalls.SetTile(leftGap2, null);
+            //delete room 0 walls
+            Vector3Int rightGap1 = new Vector3Int((gridWidth / 2) - 1, 0, 0);
+            Vector3Int rightGap2 = new Vector3Int((gridWidth / 2) - 1, -1, 0);
+            startRoomWalls.SetTile(rightGap1, null);
+            startRoomWalls.SetTile(rightGap2, null);
 
-                Vector3Int enmyLeftGap1 = new Vector3Int((enmyGridWidth / 2) - 1, 0, 0);
-                Vector3Int enmyLeftGap2 = new Vector3Int((enmyGridWidth / 2) - 1, -1, 0);
-                Debug.Log(enmyLeftGap1 + ", " + enmyLeftGap2); //shld be 6
-                wallTilemap.CompressBounds();
-                wallTilemap.SetTile(enmyLeftGap1, null);
-                wallTilemap.SetTile(enmyLeftGap2, null);
-            }
+            //find room 1 wall tilemap
+            Tilemap wallTilemap = FindTilemapWithTag(RoomInstance, wallTilemapTag);
 
-            for (int i = 0; i < 16; i++)
-            {
-                spawnPoint += direction;
-            }
-            Instantiate(roomPrefabs[0], spawnPoint, Quaternion.identity);
+            //checking room 1 wall tilemap dimensions
+            wallTilemap.CompressBounds();
+            int enmyGridWidth = wallTilemap.size.x;
+            int enmyGridHeight = wallTilemap.size.y;
+            Debug.Log(enmyGridHeight + ", " + enmyGridWidth);
+
+            //checking room 1 wall tilemap tiles to be deleted
+            Vector3Int enmyRightGap1 = new Vector3Int(-(enmyGridWidth / 2), 0, 0);
+            Vector3Int enmyRightGap2 = new Vector3Int(-(enmyGridWidth / 2), -1, 0);
+            Debug.Log(enmyRightGap1 + ", " + enmyRightGap2); // shld be
+
+            //delete appropriate room 1 walls
+            wallTilemap.CompressBounds();
+            wallTilemap.SetTile(enmyRightGap1, null);
+            wallTilemap.SetTile(enmyRightGap2, null);
+        }
+        else //ROOM ON LEFT
+        {
+            spawnPoint += (direction * 16);
+            GameObject RoomInstance = Instantiate(roomPrefabs[0], spawnPoint, Quaternion.identity);
+
+            //delete room 0 walls
+            Vector3Int leftGap1 = new Vector3Int(-(gridWidth / 2), 0, 0);
+            Vector3Int leftGap2 = new Vector3Int(-(gridWidth / 2), -1, 0);
+            startRoomWalls.SetTile(leftGap1, null);
+            startRoomWalls.SetTile(leftGap2, null);
+
+            //find room 1 wall tilemap
+            Tilemap wallTilemap = FindTilemapWithTag(RoomInstance, wallTilemapTag);
+
+            //checking room 1 wall tilemap dimensions
+            wallTilemap.CompressBounds();
+            int enmyGridWidth = wallTilemap.size.x;
+            int enmyGridHeight = wallTilemap.size.y;
+            Debug.Log(enmyGridHeight + ", " + enmyGridWidth);
+
+            //checking room 1 wall tilemap tiles to be deleted
+            Vector3Int enmyLeftGap1 = new Vector3Int((enmyGridWidth / 2) - 1, 0, 0);
+            Vector3Int enmyLeftGap2 = new Vector3Int((enmyGridWidth / 2) - 1, -1, 0);
+            Debug.Log(enmyLeftGap1 + ", " + enmyLeftGap2); //shld be 6
+
+            //delete appropriate room 1 walls
+            wallTilemap.CompressBounds();
+            wallTilemap.SetTile(enmyLeftGap1, null);
+            wallTilemap.SetTile(enmyLeftGap2, null);
         }
     }
 
